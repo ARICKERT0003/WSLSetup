a6d9462e1131606cdfa4eae3e8781a40882a3771
Alex <ARICKERT0003@gmail.com>
2024-01-15 14:18:43 -0800

    Add gruvbox-material vim theme

--- wsl_setup.sh
+++ wsl_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir ~/{Downloads,Projects}
+
 apt install g++ build-essential
 
 apt install cmake
@@ -26,9 +28,15 @@ mkdir -p ~/.vim/{autoload,backup,bundle,undo,swap}
 # NERDTree
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/autoload/nerdtree
 
-# ColorScheme 
-mkdir -p ~/.vim/pack/colors/opt
+# ColorSchemes
 
+# Everforest
+mkdir -p ~/.vim/pack/colors/opt
 git clone --depth 1 https://github.com/sainnhe/everforest.git ~/.vim/pack/colors/opt/everforest
 
+# Gruvbox-Material
+mkdir ~/.vim/colors
+git clone --depth 1 https://github.com/sainnhe/gruvbox-material.git ~/Downloads
+cp ~/Downloads/gruvbox-material/autoload/gruvbox_material.vim ~/.vim/autoload/
+cp ~/Downloads/gruvbox-material/colors/gruvbox-material.vim ~/.vim/colors/
 
